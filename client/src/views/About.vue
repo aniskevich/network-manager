b6c9a99fb91626b54dcc015cdd06c4de18030980
aniskevich <aniskevich.bo@gmail.com>
2022-01-14 17:43:03 +1000

    Room component

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,4 +2,11 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <Room></Room>
 </template>
+<script>
+import Room from '../components/Room'
+export default {
+  components: { Room }
+}
+</script>
