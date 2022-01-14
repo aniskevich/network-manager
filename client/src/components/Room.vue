90b073c978ebff79ca9b5398fcedd795be95f553
aniskevich <aniskevich.bo@gmail.com>
2022-01-14 17:42:59 +1000

    Room component

---
+++ client/src/components/Room.vue
@@ -0,0 +1,23 @@
+<template>
+  <g>
+    <line stroke="black" stroke-width={1}></line>
+    <g><text>{{ name }}</text></g>
+<!--    <g transform={`translate(${coords[0].x}, ${coords[0].y})`}></g>-->
+  </g>
+</template>
+
+<script>
+export default {
+  name: 'Room',
+  props: {
+    name: {
+      type: String,
+      default: 'test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
